5daa2334022b82f6ba0755596ec2609765692633
ianbasinger <108039268+ianbasinger@users.noreply.github.com>
2025-01-23 16:22:55 -0800

    Update libraries.h

--- libraries.h
+++ libraries.h
@@ -1,1 +1,17 @@
+#ifndef LIBRARIES_h
+#define LIBRARIES_h
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <time.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#endif
